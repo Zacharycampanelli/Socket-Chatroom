858e26a43d397cadab945f0b1fa01ae8f8a9cfdb
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-08-06 19:19:54 -0400

    sockets set up in front end and basic output to screen

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io')
+const socketio = require('socket.io');
 
 const app = express();
-const server = http.createServer(app)
-const io = socketio(server)
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when client connects
-io.on('connection', socket => {
-    console.log('New Web Socket Connection...')
-})
+io.on('connection', (socket) => {
+  // Welcome current user
+  socket.emit('message', 'Welcome to ChatCord!');
+
+  // Broadcast when a user connects
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  // Runs when a client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  });
+
+  // Listen for chatMessage
+  socket.on('chatMessage', (msg) => {
+    io.emit('message', msg);
+  })
+});
 
 const PORT = 3000 || process.env.PORT;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
